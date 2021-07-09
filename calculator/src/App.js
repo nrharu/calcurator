862136c2ad817304046d5c09995c11bbbd35fd13
nrharu <kuuasr8943@gmail.com>
2021-07-09 19:52:57 +0900

    クラス付

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,44 +1,56 @@
 function App() {
   return (
     <div className="App">
-      <div>
-        <div className="result">{/* 入力された計算式 */}</div>
-        <div>{/* 計算結果 */}</div>
+      <div className="display">
+        <div className="formula">
+          {/* 入力された計算式 */}
+          {/* <p className="test">テスト</p> */}
+        </div>
+        <div className="result_box">
+          {/* 計算結果 */}
+          {/* <p className="test_2">テスト</p> */}
+          <button className="copy_button">
+            <img src="" alt="コピーボタン" />
+          </button>
+          <div className="result">
+            <p className="result_text">結果</p>
+          </div>
+        </div>
       </div>
       <div>
         <div className="first_stage">
-          <button>c</button>
-          <button>+/-</button>
-          <button>%</button>
-          <button>/</button>
+          <button className="button">c</button>
+          <button className="button">+/-</button>
+          <button className="button">%</button>
+          <button className="button">/</button>
         </div>
         {/*  */}
         <div className="second_stage">
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>*</button>
+          <button className="button">7</button>
+          <button className="button">8</button>
+          <button className="button">9</button>
+          <button className="button">*</button>
         </div>
         {/*  */}
         <div className="third_stage">
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>-</button>
+          <button className="button">4</button>
+          <button className="button">5</button>
+          <button className="button">6</button>
+          <button className="button">-</button>
         </div>
         {/*  */}
         <div className="fourth_stage">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>+</button>
+          <button className="button">1</button>
+          <button className="button">2</button>
+          <button className="button">3</button>
+          <button className="button">+</button>
         </div>
         {/*  */}
-        <div ckassName="fifth_stage">
-          <button>0</button>
-          <button>00</button>
-          <button>.</button>
-          <button>=</button>
+        <div className="fifth_stage">
+          <button className="button">0</button>
+          <button className="button">00</button>
+          <button className="button">.</button>
+          <button className="button">=</button>
         </div>
       </div>
     </div>
